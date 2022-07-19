dfa53a41d08cf2d90682a1c6b2a299574b58e1d1
Kasib <kasibabdullah8@gmail.com>
2022-07-19 02:50:06 -0400

    shorthand slice to remove first and last char

---
+++ removeChar.py
@@ -0,0 +1,8 @@
+def remove_char(s):
+    # this is a short notation for slicing
+    # items start through stop are returned as in return
+    # just whats in between 1st char and last char
+    return s[1:-1]
+
+
+print(remove_char('Cheese'))
