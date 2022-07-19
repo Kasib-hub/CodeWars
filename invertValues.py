53feebe3decd7e03277fdaf27816352180279579
Kasib <kasibabdullah8@gmail.com>
2022-07-19 03:10:23 -0400

    invert values with list comprehension

--- invertValues.py
+++ invertValues.py
@@ -1,11 +1,6 @@
-
+# using list comphrehension - look this up
 def invert(lst):
-    for i, num in enumerate(lst):
-        if num > 0:
-            lst[i] = -num
-        elif num < 0:
-            lst[i] = abs(num)
-    return lst
+    return [-x for x in lst]
 
 arr = [1, -2, 3, -4, 5]
 print(invert(arr))
